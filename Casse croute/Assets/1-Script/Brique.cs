3d4e9ece7c7f33f7e3f8fffa7893095a5abc7d83
Kiady <kiady2227@gmail.com>
2025-03-08 00:18:10 +0100

    Build Exe

--- Casse croute/Assets/1-Script/Brique.cs
+++ Casse croute/Assets/1-Script/Brique.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem.iOS;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UIElements;
 
